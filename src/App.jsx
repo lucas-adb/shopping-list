0a2a8bf67cb1272a7f67292f613749811e44758d
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-01-02 14:44:25 -0300

    refactor: creates ShoppingItem component

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import { db } from "./config/firebase";
 import "./App.css";
 import {
   collection,
-  doc,
-  updateDoc,
+  // doc,
+  // updateDoc,
   onSnapshot,
-  deleteDoc,
+  // deleteDoc,
 } from "firebase/firestore";
 
-import { FaRegTrashCan } from "react-icons/fa6";
-import { FaEdit } from "react-icons/fa";
+// import { FaRegTrashCan } from "react-icons/fa6";
+// import { FaEdit } from "react-icons/fa";
+import ShoppingItem from "./components/ShoppingItem";
 
 const shoppingItemsCollection = collection(db, "items");
 
 function ShoppingListApp() {
   const [shoppingItems, setShoppingItems] = useState([]);
-  const [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
+  // const [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
 
   useEffect(() => {
     const stopListeningToShoppingItems = onSnapshot(
@@ -33,51 +34,21 @@ function ShoppingListApp() {
     return () => stopListeningToShoppingItems();
   }, []);
 
-  async function toggleItemCompletion(id, previousItemCompletion) {
-    const shoppingItemDoc = doc(db, "items", id);
-    await updateDoc(shoppingItemDoc, { completed: !previousItemCompletion });
-  }
+  // async function toggleItemCompletion(id, previousItemCompletion) {
+  //   const shoppingItemDoc = doc(db, "items", id);
+  //   await updateDoc(shoppingItemDoc, { completed: !previousItemCompletion });
+  // }
 
-  async function deleteItem(id) {
-    const shoppingItemDoc = doc(db, "items", id);
-    await deleteDoc(shoppingItemDoc);
-  }
+  // async function deleteItem(id) {
+  //   const shoppingItemDoc = doc(db, "items", id);
+  //   await deleteDoc(shoppingItemDoc);
+  // }
 
   return (
     <div className="shopping-list">
       <h1>Shopping List</h1>
 
-      {shoppingItems?.map((item) => (
-        <div key={item.id} className="shopping-list-item">
-          <label
-            htmlFor={item.id}
-            className={item.completed ? "item-completed" : "item-uncompleted"}
-          >
-            <input
-              type="checkbox"
-              name="checkbox"
-              id={item.id}
-              checked={item.completed}
-              onChange={() => toggleItemCompletion(item.id, item.completed)}
-            />
-            {/* {item.title} */}
-            {isEditBtnClicked ? (
-              <input type="text" placeholder="Edit..." className="edit-input"/>
-            ) : (
-              item.title
-            )}
-          </label>
-          <button
-            className="edit-btn"
-            onClick={() => setIsEditBtnClicked(!isEditBtnClicked)}
-          >
-            <FaEdit className="edit" />
-          </button>
-          <button className="delete-btn" onClick={() => deleteItem(item.id)}>
-            <FaRegTrashCan className="trash-can" />
-          </button>
-        </div>
-      ))}
+      {shoppingItems?.map((item) => <ShoppingItem key={item.id} item={item} />)}
     </div>
   );
 }
---
+++ src/components/ShoppingItem.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+import { db } from "../config/firebase";
+import {
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+import { FaRegTrashCan } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+function ShoppingItem({ item }) {
+
+  const { id, title, completed } = item;
+
+  // console.log(props);
+
+  const [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
+
+  async function toggleItemCompletion(id, previousItemCompletion) {
+    const shoppingItemDoc = doc(db, "items", id);
+    await updateDoc(shoppingItemDoc, { completed: !previousItemCompletion });
+  }
+
+  async function deleteItem(id) {
+    const shoppingItemDoc = doc(db, "items", id);
+    await deleteDoc(shoppingItemDoc);
+  }
+
+  return (
+    <div className="shopping-list-item">
+    <label
+      htmlFor={id}
+      className={completed ? "item-completed" : "item-uncompleted"}
+    >
+      <input
+        type="checkbox"
+        name="checkbox"
+        id={id}
+        checked={completed}
+        onChange={() => toggleItemCompletion(id, completed)}
+      />
+      {/* {item.title} */}
+      {isEditBtnClicked ? (
+        <input type="text" placeholder="Edit..." className="edit-input"/>
+      ) : (
+        title
+      )}
+    </label>
+    <button
+      className="edit-btn"
+      onClick={() => setIsEditBtnClicked(!isEditBtnClicked)}
+    >
+      <FaEdit className="edit" />
+    </button>
+    <button className="delete-btn" onClick={() => deleteItem(id)}>
+      <FaRegTrashCan className="trash-can" />
+    </button>
+  </div>
+  )
+}
+
+export default ShoppingItem;
+
+ShoppingItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired,
+  }).isRequired,
+};
