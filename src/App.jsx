528877d4355a6eb17b061062b679455d0033fc4c
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-01-03 15:42:07 -0300

    Refactor: replace NewItemInput with NewItemForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import ShoppingItem from "./components/ShoppingItem";
 import { getShoppingItems } from "./utils/firebaseFunctions";
 import "./App.css";
-import NewItemInput from "./components/NewItemInput";
+import NewItemForm from "./components/NewItemForm";
 
 function ShoppingListApp() {
   const [shoppingItems, setShoppingItems] = useState([]);
@@ -17,7 +17,7 @@ function ShoppingListApp() {
     <div className="shopping-list">
       <h1>Shopping List</h1>
 
-      <NewItemInput />
+      <NewItemForm />
 
       {shoppingItems?.map((item) => (
         <ShoppingItem key={item.id} item={item} />
--- src/components/NewItemForm.jsx
+++ src/components/NewItemForm.jsx
@@ -3,26 +3,39 @@ import { addNewMovie } from "../utils/firebaseFunctions";
 
 import { FaCirclePlus } from "react-icons/fa6";
 
-function NewItemInput() {
+function NewItemForm() {
   const [newItem, setNewItem] = useState("");
 
+  const isTextInputValid = (value) => {
+    return (value && value.trim() !== "")
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(isTextInputValid(newItem)) {
+      addNewMovie(newItem);
+      setNewItem("");
+    }
+  }
+
   return (
-    <div className="new-item-input-wrapper">
+    <form onSubmit={handleSubmit} className="new-item-input-wrapper">
       <input
         type="text"
         className="new-item-input"
         placeholder="Add a new Item..."
+        value={newItem}
         onChange={(event) => setNewItem(event.target.value)}
       />
       <button
-        onClick={() => addNewMovie(newItem)}
-        disabled={!(newItem && newItem.trim() !== "")}
+        disabled={!(isTextInputValid(newItem))}
         className="add-new-item-btn"
+        type="submit"
       >
         <FaCirclePlus />
       </button>
-    </div>
+    </form>
   );
 }
 
-export default NewItemInput;
+export default NewItemForm;
