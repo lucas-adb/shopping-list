4ac1ca3d1816f31626f1049f81ea484552159a13
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-01-05 17:11:52 -0300

    Add photoURL state and display profile picture in Welcome component

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,19 +1,37 @@
 import { useEffect, useState } from "react";
-import { getUsername } from "../utils/firebaseFunctions";
+import { getPhotoURL, getUsername } from "../utils/firebaseFunctions";
 
 function Welcome() {
-  const [username, setUsername] = useState([]);
+  const [username, setUsername] = useState("");
+  const [photoURL, setPhotoURL] = useState("");
+  const [isImageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
     const stopListeningToUsername = getUsername(setUsername);
+    const stopListeningToPhotoURL = getPhotoURL(setPhotoURL);
 
-    return () => stopListeningToUsername();
+    return () => {
+      stopListeningToUsername();
+      stopListeningToPhotoURL();
+    };
   }, []);
+
+  if (!(username && photoURL)) return <p>Loading...</p>
 
   return (
     <div className="welcome-wrapper">
-      <p className="username">{username}</p>
-      <p>Welcome back!</p>
+      {!isImageLoaded && <p>Loading image...</p>}
+      <img 
+        src={photoURL} 
+        alt="profile-picture" 
+        className="profile-picture" 
+        onLoad={() => setImageLoaded(true)}
+        style={{display: isImageLoaded ? 'block' : 'none'}}
+      />
+      <div className="welcome-text-wrapper">
+        <p className="username">{username}</p>
+        <p>Welcome back!</p>
+      </div>
     </div>
   );
 }
