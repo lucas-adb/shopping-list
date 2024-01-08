c4de7a6a393bbca18f52a3665eddc0be1da9c595
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-01-08 18:23:58 -0300

    Refactor SignUpPage component to include login functionality and handle photo upload

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { signUp } from "../utils/firebaseAuth";
+import { login, signUp } from "../utils/firebaseAuth";
 import { Link, useNavigate } from "react-router-dom";
 import { addNewUser } from "../utils/firebaseUsers";
 import { storePhoto } from "../utils/firebaseStorage";
@@ -27,39 +27,41 @@ function SignUpPage() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
     // validates the data
     const newErr = validateNewUser(username, password, email, previewURL);
-
     if (newErr) {
       // console.log(newErr)
       setErrorMessage(newErr);
       setErrorVisible(true);
       return
     }
-
     try {
-      // gets url from firebase
-      const photoURL = await storePhoto(previewFile);
-
       // authenticate and gets email and password
       const data = await signUp(email, password);
       const newEmail = data.user.email;
       const newUid = data.user.uid;
-
-      // adds a new user in firebase
-      await addNewUser(newEmail, newUid, username, photoURL);
-
-      // change page
-      navigate(`/mylist`);
+  
+      // login to upload photos and add new user
+      await login(email, password);
+  
+      // gets url from firebase
+      const photoURL = await storePhoto(previewFile);
+  
+      // check if photoURL is not undefined
+      if (photoURL) {
+        // adds a new user in firebase
+        await addNewUser(newEmail, newUid, username, photoURL);
+  
+        // change page
+        navigate(`/mylist`);
+      } else {
+        throw new Error("Failed to upload photo");
+      }
     } catch (error) {
       setErrorMessage("Sign up was not successful 😔");
       setErrorVisible(true);
     }
-
   };
-
-  // console.log(previewURL)
 
   return (
     <form className="shopping-list" onSubmit={handleSubmit}>
