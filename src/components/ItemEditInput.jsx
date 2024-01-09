99dfd6a49342775692b122e6e9a4994912aabc57
Lucas Alves <alvesdebrito.lucas@gmail.com>
2024-01-09 17:04:58 -0300

    Refactor item editing functionality and add error handling

--- src/components/ItemEditInput.jsx
+++ src/components/ItemEditInput.jsx
@@ -1,31 +1,39 @@
-import { useEffect, useState } from "react";
-import { updateItemTitle } from "../utils/firebaseFunctions";
+import { useState } from "react";
+import { FaSave } from "react-icons/fa";
+import PropTypes from "prop-types";
 
-import PropTypes from 'prop-types';
-
-function ItemEditInput({id}) {
+function ItemEditInput({ id, editTitle }) {
   const [newTitle, setNewTitle] = useState("");
 
-  useEffect(() => {
-    if (newTitle && newTitle.trim() !== "") {
-      updateItemTitle(id, newTitle);
-    }
-  }, [newTitle, id]);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    editTitle(newTitle);
+    setNewTitle("");
+  
+  };
+
 
   return (
-    <label htmlFor={id} className={"edit-item-label"}>
-      <input
-        type="text"
-        placeholder="Edit..."
-        className="edit-input"
-        onChange={(event) => setNewTitle(event.target.value)}
-      />
-    </label>
-  )
+      <form onSubmit={handleSubmit} className="edit-title-form">
+        <label htmlFor={id} className={"edit-item-label"}>
+          <input
+            type="text"
+            placeholder="Edit..."
+            className="edit-input"
+            onChange={(event) => setNewTitle(event.target.value)}
+          />
+        </label>
+        <button className="save-btn" type="submit">
+          <FaSave className="save-icon" />
+        </button>
+      </form>
+  );
 }
 
 export default ItemEditInput;
 
 ItemEditInput.propTypes = {
   id: PropTypes.string,
-}
+  editTitle: PropTypes.func,
+};
--- src/components/ShoppingItem.jsx
+++ src/components/ShoppingItem.jsx
@@ -4,34 +4,67 @@ import { FaRegTrashCan } from "react-icons/fa6";
 import { FaEdit } from "react-icons/fa";
 import PropTypes from "prop-types";
 
-import { deleteItem } from "../utils/firebaseFunctions";
+import { deleteItem, updateItemTitle } from "../utils/firebaseFunctions";
 import ItemCheckbox from "./ItemCheckbox";
 import ItemEditInput from "./ItemEditInput";
+import { validateNewItem } from "../validations/newItem";
 
 function ShoppingItem({ item }) {
   const { id, title, completed } = item;
 
   const [isEditBtnClicked, setIsEditBtnClicked] = useState(false);
 
-  return (
-    <div className="shopping-list-item">
-      {isEditBtnClicked ? (
-        <ItemEditInput id={id} />
-      ) : (
-        <ItemCheckbox id={id} completed={completed} title={title} />
-      )}
+  const [errorVisible, setErrorVisible] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-      <button
-        className="edit-btn"
-        onClick={() => setIsEditBtnClicked(!isEditBtnClicked)}
-      >
-        <FaEdit
-          className={isEditBtnClicked ? "edit-icon-clicked" : "edit-icon"}
-        />
-      </button>
-      <button className="delete-btn" onClick={() => deleteItem(id)}>
-        <FaRegTrashCan className="trash-can" />
-      </button>
+  const editTitle = (newTitle) => {
+    const newErr = validateNewItem(newTitle);
+
+    if (!newErr) {
+      updateItemTitle(id, newTitle);
+      setErrorVisible(false);
+
+      setIsEditBtnClicked(false);
+      setErrorMessage("");
+    } else {
+      setErrorMessage(newErr);
+      setErrorVisible(true);
+    }
+  };
+
+  const handleEditBtn = () => {
+    if (errorMessage) {
+      setErrorMessage("");
+    }
+
+    setIsEditBtnClicked(!isEditBtnClicked);
+  };
+
+  return (
+    <div className="shopping-list-wrapper">
+      <div className="shopping-list-item">
+        {isEditBtnClicked ? (
+          <ItemEditInput id={id} editTitle={editTitle} />
+        ) : (
+          <ItemCheckbox id={id} completed={completed} title={title} />
+        )}
+
+        <button
+          className="edit-btn"
+          onClick={() => handleEditBtn()}
+        >
+          <FaEdit
+            className="edit-icon"
+          />
+        </button>
+        <button className="delete-btn" onClick={() => deleteItem(id)}>
+          <FaRegTrashCan className="trash-can" />
+        </button>
+      </div>
+
+      <div className="shopping-list-item-error">
+        {errorVisible && <p className="input-error-p">{errorMessage}</p>}
+      </div>
     </div>
   );
 }
